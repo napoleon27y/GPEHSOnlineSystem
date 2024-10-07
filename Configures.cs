6974a59083a3495898e3e9e89c404691db59cc9c
영환 김 <napoleon27y@gmail.com>
2024-10-07 11:57:23 +0900

    AppSettings.json 세팅

---
+++ Configures.cs
@@ -0,0 +1,6 @@
+﻿namespace GPEHSOnlineSystem;
+
+public class ApiKeys
+{
+    public string? SfLicense { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,24 @@
+using GPEHSOnlineSystem;
 using GPEHSOnlineSystem.Components;
+using Microsoft.Extensions.Options;
+using Syncfusion.Licensing;
+using System.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//SyncfusionLicenseProvider.RegisterLicense("MzUxNTc3OUAzMjM3MmUzMDJlMzBLUEswUnBLbnZIZE8vb1EzZ1dQN2MzeEl4RDBtMTRwRFRZaFB1bEV1TUxFPQ==");
+
 // Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+//builder.Services.Configure<ApiKeys>(builder.Configuration.GetSection("ApiKeys"));
+
+var sfKey = builder.Configuration.GetSection("ApiKeys").Get<ApiKeys>();
+Debug.WriteLine("==========================");
+Debug.WriteLine(sfKey?.SfLicense);
+Debug.WriteLine("==========================");
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
